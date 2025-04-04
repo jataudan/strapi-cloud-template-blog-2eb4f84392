8c6c57cdbc6300955cd86d36796efc6a92d8e19e
ShairAli10 <shairaliyamin@gmail.com>
2025-04-04 19:42:47 +0500

    Updated the content types names and removed unused types

--- src/api/keyword-research/routes/keyword-research.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * keyword-research router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::keyword-research.keyword-research');
